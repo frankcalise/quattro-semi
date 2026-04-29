1c933f078bed1a06b23baea619e38e6db7890e23
Frank Calise <fcalise@gmail.com>
2026-04-29 13:28:16 -0400

    Scaffold Expo CNG foundation

---
+++ app.config.ts
@@ -0,0 +1,39 @@
+import type { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "Quattro Semi",
+  slug: "quattro-semi",
+  scheme: "quattrosemi",
+  version: "0.1.0",
+  orientation: "portrait",
+  userInterfaceStyle: "automatic",
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.quattrosemi.game"
+  },
+  android: {
+    package: "com.quattrosemi.game"
+  },
+  experiments: {
+    typedRoutes: true
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-build-properties",
+      {
+        ios: {
+          newArchEnabled: true
+        },
+        android: {
+          newArchEnabled: true
+        }
+      }
+    ]
+  ],
+  extra: {
+    enableDebugRoutes: process.env.EXPO_PUBLIC_ENABLE_DEBUG_ROUTES !== "false"
+  }
+};
+
+export default config;
